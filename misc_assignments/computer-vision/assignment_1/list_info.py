7803ceecbf99accb11f5e5e52da32eccb7296600
Jonathon McNabb <jmcnabb@mines.edu>
2020-08-29 15:43:06 -0600

    finished assign 1

---
+++ misc_assignments/computer-vision/assignment_1/list_info.py
@@ -0,0 +1,56 @@
+import sys
+
+with open('datafile.txt', 'r') as f:
+    b = eval(f.read())
+
+    maxVal = -sys.maxint - 1
+    minVal = sys.maxint
+
+    index = -1
+
+    length = len(b)
+
+    for i in range(length):
+        if b[i] > maxVal:
+            maxVal = b[i]
+        if b[i] < minVal:
+            minVal = b[i]
+        if b[i] == 38:
+            index = i
+
+    b.sort()
+
+    frequency = 1
+    count = 0
+    values = []
+    for i in range(length-1):
+        if b[i+1] == b[i]:
+            frequency += 1
+        else:
+            if frequency > count:
+                count = frequency
+                values = []
+                values.append(b[i])
+            elif frequency == count:
+                values.append(b[i])
+            frequency = 1
+
+    if frequency > count:
+        count = frequency
+        values = []
+        values.append(b[i])
+    elif frequency == count:
+        values.append(b[i])
+        print values
+
+    evens = []
+    for i in range(length):
+        if b[i] % 2 == 0:
+            evens.append(b[i])
+
+    print('Max: ' + str(maxVal))
+    print('Min: ' + str(minVal))
+    print('38 is stored at index: ' + str(index))
+    print('Most common: ' + str(values) + ' was repeated ' + str(count) + ' times.')
+    print('Sorted: ' + str(b))
+    print('Evens: ' + str(evens))
---
+++ misc_assignments/computer-vision/assignment_1/state_machine_abcd.py
@@ -0,0 +1,63 @@
+def state0(character):
+    if character != 'a':
+        return state0
+    elif character == 'a':
+        return statea
+
+def statea(character):
+    if character == 'a':
+        return statea
+    elif character == 'b':
+        return stateb
+    elif character != 'a' or character != 'b':
+        return state0
+
+
+def stateb(character):
+    if character == 'a':
+        return statea
+    elif character == 'c':
+        return statec
+    elif character != 'a' or character != 'c':
+        return state0
+
+
+def statec(character):
+    if character == 'a':
+        return statea
+    elif character == 'd':
+        return stated
+    elif character != 'a' or character != 'd':
+        return state0
+
+
+def stated(character):
+    print "abcd is contained in the string"
+    if character == 'a':
+        return statea
+    elif character != 'a':
+        return state0
+
+# create a dictionary to describe the states
+state_dictionary = {
+    state0 : "State 0",
+    statea : "State A",
+    stateb : "State B",
+    statec : "State C",
+    stated : "State D"
+}
+
+
+state = state0 # initial state as pointer to state0 function
+print("Type any string")
+print("State machine will detect if abcd is contained in the string")
+input_string = raw_input("Enter String: ")
+input_string = list(input_string)
+
+length = len(input_string)
+
+for i in range(length): # Run until state is None
+    new_state = state(input_string[i]) # launch state machine
+    state = new_state # update the next state
+new_state = state(input_string[0]) # launch state machine
+print("Done with state machine")
