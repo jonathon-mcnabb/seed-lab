b974cbfdacf38e3fe3afdaf7fc048c49de60f3dd
Jonathon McNabb <jmcnabb@mines.edu>
2020-08-29 16:46:28 -0600

    emails working" git push

--- notify_on_startup/notify_ip.py
+++ notify_on_startup/notify_ip.py
@@ -2,10 +2,7 @@ import smtplib
 import os
 
 #'\' is used to splite pythone line
-ipaddress = os.popen("ifconfig wlan0 \
-                     | grep 'inet addr' \
-                     | awk -F: '{print $2}' \
-                     | awk '{print $1}'").read()
+ipaddress = os.popen("hostname -I").read()
 ssid = os.popen("iwconfig wlan0 \
                 | grep 'ESSID' \
                 | awk '{print $4}' \
