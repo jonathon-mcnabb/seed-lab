a78bd260be89b34f84950c53cd1cbfb7ac71df17
Jonathon McNabb <jmcnabb@mines.edu>
2020-08-29 16:24:23 -0600

    added script to send email of ip on raspberry pi startup

---
+++ notify_on_startup/notify_ip.py
@@ -0,0 +1,38 @@
+import smtplib
+import socket
+
+try:
+    host_name = socket.gethostname()
+    host_ip = socket.gethostbyname(host_name)
+    print("IP : ", host_ip)
+except:
+    print("Unable to get Hostname and IP")
+
+    # Driver code
+
+TO = 'jmcnabb@mymail.mines.edu'
+SUBJECT = 'SSH Command'
+TEXT = 'Please run the following command to ssh into the raspberry pi: \n\n' \
+       'ssh pi@' + str(host_ip)
+
+# Gmail Sign In
+gmail_sender = 'EMAIL_USERNAME'
+gmail_passwd = 'EMAIL_PASSWORD!'
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(gmail_sender, gmail_passwd)
+
+BODY = '\r\n'.join(['To: %s' % TO,
+                    'From: %s' % gmail_sender,
+                    'Subject: %s' % SUBJECT,
+                    '', TEXT])
+
+try:
+    server.sendmail(gmail_sender, TO, BODY)
+    print('email sent')
+except:
+    print('error sending mail')
+
+server.quit()
