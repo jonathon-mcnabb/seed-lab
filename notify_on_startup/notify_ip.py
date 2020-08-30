36b7ad7500306d2609402ed2ec5bf43e101ca7fb
Jonathon McNabb <jmcnabbdev@gmail.com>
2020-08-29 18:43:47 -0600

    modified startup file to wait for network connection

--- notify_on_startup/notify_ip.py
+++ notify_on_startup/notify_ip.py
@@ -1,5 +1,19 @@
 import smtplib
 import os
+import subprocess
+import time
+
+t = False
+waiting = True
+while waiting:
+    counter = 0
+    try:
+        t = subprocess.check_output(["ping", "-c", "1", "google.com"])
+    except:
+        waiting = True
+    if t:
+        waiting = False
+    time.sleep(1)
 
 #'\' is used to splite pythone line
 ipaddress = os.popen("hostname -I").read()
@@ -11,14 +25,17 @@ ssid = os.popen("iwconfig wlan0 \
 print("ssid: " + ssid)
 print("ipaddress: " + ipaddress)
 host_ip = ipaddress
-TO = 'EMAIL_SENDING_TO'
+TO = 'jmcnabb@mymail.mines.edu'
 SUBJECT = 'SSH Command'
 TEXT = 'Please run the following command to ssh into the raspberry pi: \n\n' \
-       'ssh pi@' + str(host_ip)
+       'ssh pi@' + str(host_ip) + '\n' \
+       'ssid: ' + str(ssid)
+       
+
 
 # Gmail Sign In
-gmail_sender = 'EMAIL_USERNAME'
-gmail_passwd = 'EMAIL_PASSWORD!'
+gmail_sender = 'jmcnabbdev@gmail.com'
+gmail_passwd = 'Celloforever5!'
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.ehlo()
