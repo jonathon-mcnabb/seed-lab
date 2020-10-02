fe7722bbb3dcaa6a66dfc56946a63360175f41c5
Greg Meklo <gmeklo@mymail.mines.edu>
2020-10-02 13:53:45 -0600

    Pi and Arduino interaction and LCD display added

--- deliverables/mini_project/4_2/rad.py
+++ deliverables/mini_project/4_2/rad.py
@@ -5,6 +5,36 @@ import time
 import cv2
 import numpy as np
 from cv2 import aruco
+import smbus
+import board
+import busio
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+# Create the bus connection
+bus = smbus.SMBus(1)
+
+# This is the address we setup in the Arduino Program
+address = 0x04
+
+# Character LCD size
+lcd_columns = 16
+lcd_rows = 2
+
+# Initialise I2C bus.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Initialise the LCD class
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+lcd.clear()
+
+# Set LCD color to white
+lcd.color = [10, 10, 10]
+time.sleep(1)
+
+# function to write value to the arduino
+def writeNumber(value):
+    bus.write_byte(address, value)
+    return -1
 
 def aruco_detection():    
     # start video capture for distance
@@ -76,8 +106,10 @@ def aruco_detection():
                 print(quadrant)
 
             # GREG ADD HERE
-            
-            
+            if sendStuff == True:
+                writeNumber(quadrant)
+                lcd.message = "Setpoint: " + str(quadrant)
+                sendStuff = False
             #
         cv2.imshow('frame',gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
