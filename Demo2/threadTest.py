1480e8730a12589af7a87348b3c7266704309525
Jonathon McNabb <jmcnabbdev@gmail.com>
2020-11-09 10:04:02 -0700

    updated thread test

--- Demo2/threadTest.py
+++ Demo2/threadTest.py
@@ -13,7 +13,7 @@ def write_to_serial(ser, value):
         #print("value", value)
         ser.write(value.encode())
         # ser.write(1)
-        time.sleep(3)
+        time.sleep(2)
         #time.sleep(6)
         #print("After write")
         lock.release()
@@ -30,6 +30,7 @@ def read_from_arduino(ser):
             line = ser.readline().decode('utf-8').rstrip()
             
             print("Arduino: ", line)
+        #time.sleep(0.01)
         #ser.write(str(value).encode('utf-8'))
     except:
         print("READ Error")
@@ -50,10 +51,15 @@ while True:
     for thread in threading.enumerate():
         #print(thread.getName())
         thread_list.append(thread.getName())
+        
+    #print (thread_list)
     if "send" not in thread_list:
+        #print("adding send thread")
         t1 = threading.Thread(target=write_to_serial, name="send", args=(ser, value_to_send,))
         t1.start()
     if "receive" not in thread_list:
+        #print("adding write thread")
+        
         t2 = threading.Thread(target=read_from_arduino, name="receive", args=(ser,))
         t2.start()
 
