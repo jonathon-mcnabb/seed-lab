c4e71d80629f80049ff73e8ac268f88bc76475eb
Jonathon McNabb <jmcnabbdev@gmail.com>
2020-11-15 15:12:49 -0700

    began adding state machine to python code. Still need to parse from network_read and test

--- finalDemo/fastCV.py
+++ finalDemo/fastCV.py
@@ -7,6 +7,7 @@ from imutils.video.pivideostream import PiVideoStream
 from imutils.video import FPS
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+import numpy as np
 import argparse
 import imutils
 import time
@@ -28,6 +29,9 @@ time.sleep(3)
 
 last_sent = 0
 state = "a"
+
+# constants
+box_radius = 0.3048 # 1 foot in meters
 
 def write_to_i2c(bus, value, frame_number):
     global this_pi
@@ -60,7 +64,6 @@ def send_to_server(msg):
     UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
 def read_from_client():
-    #socket2.bind(clientAddressPort)
     data, addr = socket2.recvfrom(1024)
     return data
 
@@ -73,12 +76,14 @@ def process_frame(frame, frame_number):
         print("[INFO] skipping frame in process_frame")
         return
     corners, ids, _ = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
+    x_angle = None
+    finalDistance = None
     if len(corners):
         global this_pi
         height, width, _ = frame.shape
         value_to_send = ""
-        global state
-        if state is "a":
+        global request
+        if request is "a":
             middle_x = int(width / 2)
             middle_y = int(height / 2)
             marker_one = corners[0][0]
@@ -118,7 +123,7 @@ def process_frame(frame, frame_number):
                 x_angle = -1*(right_offset - x_angle)
                 value_to_send = 'AR' + str(round(x_angle, 4))
             print(value_to_send)
-        elif state is "d":
+        elif request is "d":
             deltaY1 = abs(cornerFour[1] - cornerOne[1])
             deltaY2 = abs(cornerThree[1] - cornerTwo[1])
 
@@ -135,6 +140,63 @@ def process_frame(frame, frame_number):
             if this_pi != "middle" and frame_number % 3 == 0:
                 t1 = threading.Thread(target=send_to_server, name="send", args=(value_to_send,))
                 t1.start()
+
+    if frame_number % 3 == 0:
+        if this_pi == "middle":
+            print(read_from_client())
+            leftAngle = None
+            rightAngle = None
+            middleAngle = x_angle
+
+            leftDistance = None
+            rightDistance = None
+            middleDistance = finalDistance
+
+            # parse though read_from_client()
+            EXTRA_ANGLE = np.radians(0)
+            angle_to_spin_while_finding = 1.5708 + EXTRA_ANGLE # right turn 90 deg in radians + EXTRA_ANGLE
+
+            # this is where we actually are going to handle states
+
+            # i is the state to find the first beacon
+            if state = "i":
+                # initial marker
+
+                # 1) spin clockwise until initial marker is found
+                # 2) move to P1 (offset by angle)
+                # P1 == (theta_i = tan^-1(r/d), distance = sqrt(r^2+d^2)
+                # 3) Spin right so that full FOV can be seen at once
+                # rotate so edge of camera FOV aligns with a 90 deg angle
+                # theta_s -> theta search
+                # theta_s = 30 deg - theta_i + EXTRA_ANGLE
+                # if we need to add more range to the FOV after turning, modify extra angle
+
+                # if an angle exists
+                if (leftAngle is not None) or (rightAngle is not None) or (middleAngle is not None):
+                    # get P1
+                    # we want to take the biggest point
+                    angleArray = [leftAngle, rightAngle, middleAngle]
+                    distanceArray = [leftDistance, rightDistance, middleDistance]
+                    biggestAngleIndex = np.argmax(angleArray) # finds index of maximum angle
+
+                    # find the angle and distance to move towards the 1st beacon
+                    theta = round(np.arctan(box_offset/distanceArray[biggestAngleIndex]) + angleArray[biggestAngleIndex], 4) # finds theta_offset to move to correct point. Made up of angle to box + offset angle
+                    distance_to_move = round(np.sqrt(distanceArray[biggestAngleIndex]**2 + box_offset**2), 4)
+
+                    theta2 = round(1.5708 - theta ,4) # after we move to P1, reset angle to 90 deg (1.5708)
+                    # send the
+                    value_to_send = 'A' + theta + 'SD' + distance_to_move+ 'S' + 'A' + theta2 + 'SD0S'  # send P1 as well as next angle to turn
+                    t1 = threading.Thread(target=write_to_i2c, name="send", args=(bus, value_to_send, frame_number))
+                    t1.start()
+                else:
+                    value_to_send = 'A' + angle_to_spin_while_finding + 'SD0S' # send the angle and also send 0 distance
+                    t1 = threading.Thread(target=write_to_i2c, name="send", args=(bus, value_to_send, frame_number))
+                    t1.start()
+
+
+            # b state is to find all other beacons
+            elif state = "b":
+                pass
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -194,7 +256,8 @@ fps = FPS().start()
 # loop over some frames...this time using the threaded stream
 
 frame_number = 0
-state = "a"
+state = "i"
+request = "a"
 if args["test"] is 1:
     while fps._numFrames < args["num_frames"]:
 	    # grab the frame from the threaded video stream and resize it
@@ -222,9 +285,7 @@ else:
             cv2.imshow("Frame", small_frame)
             key = cv2.waitKey(1) & 0xFF
         frame_number = frame_number + 1
-        if frame_number % 3 == 0:
-            if this_pi == "middle":
-                print(read_from_client())
+
 # stop the timer and display FPS information
 fps.stop()
 if args["log"] > 0:
@@ -234,3 +295,17 @@ if args["log"] > 0:
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
+
+
+# Notes
+
+
+
+# All other markers
+
+#1) check if current marker can be seen
+    # if a marker can be seen, choose the most right marker
+
+# if marker cannot be seen, we may need to move around the box
+#2) if marker cannot be seen
+    # spin 90 deg
